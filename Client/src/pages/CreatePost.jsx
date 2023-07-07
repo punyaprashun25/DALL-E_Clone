0dbe4f0e719bb938ce5f1a563b2b20f567f31abb
Punya Prashun <kumar.abhishek.cs24@iilmcet.ac.in>
2023-07-07 15:05:58 +0530

    server-link-changed

--- Client/src/pages/CreatePost.jsx
+++ Client/src/pages/CreatePost.jsx
@@ -27,7 +27,7 @@ const CreatePost = () =>
         setLoading(true);
         try
         {
-          const response = await fetch('https://dall-e-hbn3.onrender.com/api/v1/post', {
+          const response = await fetch('https://dall-e-c85v.onrender.com/api/v1/post', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
@@ -59,7 +59,7 @@ const CreatePost = () =>
         try
         {
           setGeneratingImg(true);
-          const response = await fetch('https://dall-e-hbn3.onrender.com/api/v1/dalle', {
+          const response = await fetch('https://dall-e-c85v.onrender.com/api/v1/dalle', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
